437e8d8b74f776dfa391bc45ae77e094f0a9e81d
Khadija Mohamed <khadijam@live.ca>
2022-01-21 02:55:14 -0500

    index2

---
+++ index2.js
@@ -0,0 +1,11 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
